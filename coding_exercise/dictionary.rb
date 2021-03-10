0178b626ca566920ae6e7c421e356bea870c69d6
Marnoel de Guzman <marnoeldeguzman@gmail.com>
2021-03-10 17:52:18 +0800

    First Commit

--- coding_exercise/dictionary.rb
+++ coding_exercise/dictionary.rb
@@ -1,3 +1,11 @@
+# Activity:
+# 1. Create a dictionary (hash) with 10 city names, where the city name (key) will be a string, and the area code would be the value.
+# 2. Display the city names to the user for cities which are available in the dictionary
+# 3. Get input from the user on the city name
+# 4. Display area code based on user's city choice
+# 5. Loop - keep the program running and prompt the user for new city names to lookup
+# 6. Complete the two methods to lookup city names and to find area code based on city names
+
 dial_book = {
     "newyork" => "212",
     "newbrunswick" => "732",
